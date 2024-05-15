f69fe8cbf582fced82a3f685ce71984995ab2f3b
Aurelien FERNANDEZ <aurelien.fernandez@algosup.com>
2024-05-15 14:20:33 +0200

    Feat: Card swipe on match page
    Cards are now displayed on the match page and can be swiped on either: left, top and right.

--- src/job_matching_app/lib/match_page.dart
+++ src/job_matching_app/lib/match_page.dart
@@ -1,13 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_card_swiper/flutter_card_swiper.dart';
 
 class MatchPage extends StatelessWidget {
   const MatchPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Match Page'),
+    List<Container> cards = [
+      Container(
+        alignment: Alignment.center,
+        color: Colors.blue,
+        child: const Text('1'),
+      ),
+      Container(
+        alignment: Alignment.center,
+        color: Colors.red,
+        child: const Text('2'),
+      ),
+      Container(
+        alignment: Alignment.center,
+        color: Colors.purple,
+        child: const Text('3'),
+      )
+    ];
+
+    return Scaffold(
+      body: CardSwiper(
+        cardsCount: cards.length,
+        allowedSwipeDirection: const AllowedSwipeDirection.only(
+          left: true,
+          up: true,
+          right: true,
+          down: false,
+        ),
+        cardBuilder: (context, index, percentThresholdX, percentThresholdY) =>
+            cards[index],
       ),
     );
   }
