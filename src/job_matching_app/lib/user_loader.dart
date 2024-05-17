8da4e0c00dd2a7d0cd070610b46a292ff141c07f
Aurelien FERNANDEZ <aurelien.fernandez@algosup.com>
2024-05-17 14:18:22 +0200

    Feat: connection page
    The connection page has been created and is currently working, it is not yet implemented as the first page to avoid loosing time when tests occurs.

--- src/job_matching_app/lib/user_loader.dart
+++ src/job_matching_app/lib/user_loader.dart
@@ -5,40 +5,58 @@ import 'dart:convert';
 import 'package:flutter/services.dart';
 
 //------------------------- USER PARSER -------------------------
-Future<void> loadUser() async {
+Future<int> getUser(String email, String password) async {
   final String jsonString =
-      await rootBundle.loadString('lib/assets/json/user.json');
+      await rootBundle.loadString('lib/assets/json/users.json');
   var data = jsonDecode(jsonString);
   // Parse user data
-  var userJson = data['user'];
+  var userJson = data['users'];
+
+  var userInfo = userJson.firstWhere(
+    (user) =>
+        user['email'].toLowerCase() == email.toLowerCase() &&
+        user['password'].toLowerCase() == password.toLowerCase(),
+    orElse: () => 0,
+  );
+  if (userInfo == 0) {
+    return userInfo;
+  }
   // Get the softskills list
-  List<dynamic> softSkillsJson = userJson['softSkills'];
-  List<List<int>> softSkills = softSkillsJson.map<List<int>>((skills) {
+  List<List<int>> softSkills = userInfo['softSkills'].map<List<int>>((skills) {
     return List<int>.from(skills);
   }).toList();
 
+  // Get the chart list
+  List<int> chart = userInfo['chart']
+      .map((chart) => int.tryParse(chart.toString()))
+      .where((parsedValue) => parsedValue != null)
+      .cast<int>()
+      .toList();
+
   // Initialize the User instance
   User.initialize(
-    firstname: userJson['firstname'] ?? '',
-    lastName: userJson['lastname'] ?? '',
-    email: userJson['email'] ?? '',
+    email: userInfo['email'].toString(),
+    password: userInfo['password'].toString(),
     softSkills: softSkills,
+    chart: chart,
   );
+  return 1;
 }
 
+//---------------------------- USER ---------------------------
 class User {
-  final String firstname;
-  final String lastName;
   final String email;
+  final String password;
   final List<List<int>> softSkills;
+  final List<int> chart;
 
   static User? _instance;
 
   User(
-      {required this.firstname,
-      required this.lastName,
-      required this.email,
-      required this.softSkills});
+      {required this.email,
+      required this.password,
+      required this.softSkills,
+      required this.chart});
 
   static User getInstance() {
     if (_instance == null) {
@@ -48,19 +66,19 @@ class User {
   }
 
   static void initialize({
-    required String firstname,
-    required String lastName,
     required String email,
+    required String password,
     required List<List<int>> softSkills,
+    required List<int> chart,
   }) {
     if (_instance != null) {
       throw Exception("User already initialized.");
     }
     _instance = User(
-      firstname: firstname,
-      lastName: lastName,
       email: email,
+      password: password,
       softSkills: softSkills,
+      chart: chart,
     );
   }
 }
