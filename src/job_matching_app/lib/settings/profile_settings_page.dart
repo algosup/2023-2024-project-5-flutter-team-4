9ee36064d3730809d4c5597c7c8f41ac3445f905
laurentbouquin <laurent.bouquin@algosup.com>
2024-06-04 16:58:28 +0200

    Update profile_settings_page.dart
    - Add possibility to change name for companies.

--- src/job_matching_app/lib/settings/profile_settings_page.dart
+++ src/job_matching_app/lib/settings/profile_settings_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:job_matching_app/settings/more_settings_page.dart';
 
+import 'package:cloud_firestore/cloud_firestore.dart';
+
 class ProfileSettingsPage extends StatefulWidget {
   const ProfileSettingsPage({super.key});
 
@@ -12,24 +14,36 @@ class ProfileSettingsPage extends StatefulWidget {
 class _ProfileSettingState extends State<ProfileSettingsPage> {
   bool isDarkMode = false;
   bool isCompanyView = false;
-  String Title = "";
 
-  
+  int companyID = 0;
+
+  String name = "";
+
+  String docName = "";
+
+  FirebaseFirestore db = FirebaseFirestore.instance;
+
   @override
   void initState() {
     super.initState();
-    getSharedPreferences('isCompanyView').then((value) {
-      isCompanyView = value;
-      if (value){
-        Title = "Company Profile Settings";
-      }
-      else {
-        Title = "Candidate Profile Settings";
-      }
-    }).then((value) => setState(() {
-      
-    }));
-    }
+
+    db = FirebaseFirestore.instance;
+
+    db.collection('Companies').get().then(
+      (querySnapshot) {
+        for (var result in querySnapshot.docs) {
+          if (result.data()["ID"] == companyID ){
+            if (result.data()["Name"] != null) {
+              name = result.data()["Name"];
+            }
+            break;
+          }
+        }
+      },
+    ).then(
+      (value) => setState(() {}),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -38,19 +52,22 @@ class _ProfileSettingState extends State<ProfileSettingsPage> {
         backgroundColor: Theme.of(context).colorScheme.background,
         automaticallyImplyLeading: true,
         foregroundColor: Theme.of(context).colorScheme.tertiary,
-        title: Text(Title,
-            style: const TextStyle(
+        title: const Text("Your Profile",
+            style: TextStyle(
               color: Colors.black,
             )),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            ElevatedButton(
+        actions: [
+          Container(
+            margin: const EdgeInsets.only(right: 10.0),
+            child: ElevatedButton(
               style: ButtonStyle(
                 backgroundColor: MaterialStateProperty.all(
                     Theme.of(context).colorScheme.secondary),
+                shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                  RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                ),
               ),
               onPressed: () {
                 Navigator.push(
@@ -60,9 +77,63 @@ class _ProfileSettingState extends State<ProfileSettingsPage> {
                   ),
                 );
               },
-              child: const Icon(Icons.settings_suggest_rounded, color: Colors.black),
+              child: const Icon(
+                Icons.settings_suggest_rounded,
+                color: Colors.black,
+              ),
+            ),
+          ),
+        ],
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10.0),
+                color: Theme.of(context).colorScheme.secondary,
+              ),
+              width: MediaQuery.of(context).size.width * 0.8,
+              child: TextField(
+                controller: TextEditingController(
+                  text: name.isEmpty ? "" : name,
+                ),
+                onChanged: (value) => name = value,
+                decoration: const InputDecoration(
+                  border: InputBorder.none,
+                  labelText: 'Name',
+                  hintText: 'Enter your name',
+                ),
+              ),
             ),
           ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Theme.of(context).colorScheme.secondary,
+        onPressed: () {
+          db
+              .collection('Companies')
+              .where('ID', isEqualTo: companyID)
+              .get()
+              .then(
+                (querySnapshot) {
+                  docName = querySnapshot.docs[0].id;
+                },
+              )
+              .then(
+                (value) => db.collection('Companies').doc(docName).update({
+                  "Name": name,
+                }),
+              )
+              .then(
+                (value) => setState(() {}),
+              );
+        },
+        child: const Icon(
+          Icons.save,
+          color: Colors.black,
         ),
       ),
     );
